0c6472595c53dc051579e85f4fd8c885bcb79f60
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-02 18:56:24 +0530

    cards created

--- src/card.js
+++ src/card.js
@@ -1,3 +1,5 @@
+import './card.css'
+
 export default function MenuCard(props){
     const {title , imagUrl, price, rating, isveg} = props;
     return(
@@ -5,9 +7,8 @@ export default function MenuCard(props){
            <img src = {imagUrl} alt="imageUrl" className="food-card"/>
            <div className="food_info">
               <h2 className="food_title">{title}</h2>
-              <h2 className="check">{isveg}</h2>
-              <p className="food_price">{price}</p>
-              <p className="food_rating">{rating}</p>
+              <p className="food_price">Price:{price}</p>
+              <p className="food_rating">Ratings:{rating}</p>
            </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import {createRoot}  from  "react-dom/client";
 import MenuCard from "./card";
+import chinese from "./images/Chinese_items.jpg";
+import south_indian from "./images/south_indian.avif";
+import marathi_food from "./images/maharastrian_food.avif";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
     <>
-      <MenuCard title="Chinese items" imagUrl="./src/images/Chinese_items.jpg" isVeg="yes" price={250}  rating={4.5}/>
-      <MenuCard title="South Indian" imagUrl="./src/images/south_indian.avif" isVeg="yes" price={200}  rating={5}/>
+      <MenuCard title="Chinese items" imagUrl={chinese} price={250}  rating={4.5}/>
+      <MenuCard title="South Indian" imagUrl={south_indian} price={200}  rating={5}/>
+      <MenuCard title="Maharashtrian Food" imagUrl={marathi_food} price={150}  rating={4.7}/>
     </>
 )
